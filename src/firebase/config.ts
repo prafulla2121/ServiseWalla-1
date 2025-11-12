9aeaac37b68733d435069ea1214ca4624782a264
Neha Purohit <nehapurohit917@gmail.com>
2025-11-12 18:42:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1093772561-39bbb",
+  "appId": "1:314425925714:web:83f9b382121cb22e127f8b",
+  "apiKey": "AIzaSyBSKd7aPcZhMrEL0czK6IbkqbEutfkqXkc",
+  "authDomain": "studio-1093772561-39bbb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "314425925714"
+};
